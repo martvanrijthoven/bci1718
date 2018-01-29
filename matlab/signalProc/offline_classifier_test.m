2187ae0fadbc56921346ebd851b260ccbc434d5c
EmielStoelinga <emielstoelinga@gmail.com>
2018-01-29 20:34:00 +0100

    start with test

--- matlab/signalProc/offline_classifier_test.m
+++ matlab/signalProc/offline_classifier_test.m
@@ -10,4 +10,11 @@ addpath(fullfile(util_folder));
 load '../../data/training_data_test_180117_Emiel1015.mat'
 
 capFile='cap_project';
-clsfr=buffer_train_ersp_clsfr(traindata,traindevents,hdr,'spatialfilter','car','freqband',[6 8 17 19],'capFile',capFile,'overridechnms',1);
+clsfr=buffer_train_ersp_clsfr(traindata,traindevents,hdr,'spatialfilter','car','freqband',[6 8 17 19],'capFile',capFile,'overridechnms',1);clsfr=buffer_train_ersp_clsfr(traindata,traindevents,hdr,'spatialfilter','car','freqband',[6 8 17 19],'capFile',capFile,'overridechnms',1);
+
+%%
+load '../../data/training_data_test_180118_Emiel1015.mat'
+
+output=buffer_apply_ersp_clsfr(traindata,clsfr)
+
+% include performance test
