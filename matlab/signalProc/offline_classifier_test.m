a799268cc47b6bde5b29c68ff692e4e4f676fd69
EmielStoelinga <emielstoelinga@gmail.com>
2018-01-31 13:14:50 +0100

    include performance script

--- matlab/signalProc/offline_classifier_test.m
+++ matlab/signalProc/offline_classifier_test.m
@@ -7,14 +7,34 @@ end
 
 util_folder = '../utilities';
 addpath(fullfile(util_folder));
-load '../../data/training_data_test_180117_Emiel1015.mat'
+load '../../data/training_data_test_Emiel1015_firstThree.mat'
 
 capFile='cap_project';
 clsfr=buffer_train_ersp_clsfr(traindata,traindevents,hdr,'spatialfilter','car','freqband',[6 8 17 19],'capFile',capFile,'overridechnms',1);clsfr=buffer_train_ersp_clsfr(traindata,traindevents,hdr,'spatialfilter','car','freqband',[6 8 17 19],'capFile',capFile,'overridechnms',1);
 
 %%
-load '../../data/training_data_test_180118_Emiel1015.mat'
+load '../../data/training_data_test_180124_Emiel1015.mat'
 
-output=buffer_apply_ersp_clsfr(traindata,clsfr)
+[f,fraw,p,X]=buffer_apply_ersp_clsfr(traindata,clsfr);
 
-% include performance test
+%% include performance test
+
+% now f<0 is taken as the left target, not sure if that is correct ^Emiel
+for idx = 1:numel(f)
+    if f(idx)<=0
+        f(idx)=1;
+    else
+        f(idx)=2;
+    end
+end
+
+output = [];
+labels=extractfield(traindevents,'value');
+for idx = 1:numel(f)
+    if f(idx)==labels(idx)
+        output(idx)=1
+    else
+        output(idx)=0
+    end
+end
+mean(output)
